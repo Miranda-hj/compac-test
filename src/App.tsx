dc1d20469181244eb9d49ca64309cb334978bca9
Miranda-hj <74184412+Miranda-hj@users.noreply.github.com>
2022-04-08 14:00:38 +1200

    set up get

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import styles from "./App.module.scss";
-import { Input } from "./components/Input";
+import { Table } from "./components/Table";
 
 export default function App() {
   const [list, setList] = useState([]);
@@ -12,24 +12,48 @@ export default function App() {
       .then((response) => response.json())
       .then((data) => setList(data));
   }, []);
-  // const {
-  //   register,
-  //   handleSubmit,
-  //   watch,
-  //   formState: { errors },
-  // } = useForm();
-  // const onSubmit = (data: any) => console.log(data);
+  console.log(list);
   return (
     <div className={styles.basic}>
-      {/* <form onSubmit={handleSubmit(onSubmit)}> */}
-      {list.map((data, i) => (
-        <>
-          <Input label={data} key={i} />
-        </>
-      ))}
+      <table>
+        <tr className={styles.header}>
+          <th>id</th>
+          <th>customerId</th>
+          <th>siteId</th>
+          <th>registrationId</th>
+          <th>name</th>
+          <th>hardware variant</th>
+          <th>authorisation mode</th>
+          <th>authorisation timeout</th>
+          <th>authorisation flags</th>
+          <th>payment gateway address</th>
+          <th>currency</th>
+          <th>printer type</th>
+          <th>passcode</th>
+        </tr>
 
-      {/* <input type="submit" /> */}
-      {/* </form> */}
+        {list.map((data, i) => (
+          <tr className={styles.content}>
+            <Table
+              key={i}
+              id={data.id}
+              customerId={data.customerId}
+              siteId={data.siteId}
+              registrationId={data.registrationId}
+              name={data.name}
+              description={data.description}
+              hardwareVariant={data.hardwareVariant}
+              authorisationMode={data.authorisationMode}
+              authorisationTimeout={data.authorisationTimeout}
+              authorisationFlags={data.authorisationFlags}
+              paymentGatewayAddress={data.paymentGatewayAddress}
+              currency={data.currency}
+              printerType={data.printerType}
+              passcode={data.passcode}
+            />
+          </tr>
+        ))}
+      </table>
     </div>
   );
 }
