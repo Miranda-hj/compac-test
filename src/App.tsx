73a7d082b7ba9041d884b02bc1629cccdd72ba24
Miranda-hj <74184412+Miranda-hj@users.noreply.github.com>
2022-04-08 13:07:02 +1200

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
 import styles from "./App.module.scss";
+import { Input } from "./components/Input";
 
 export default function App() {
-  return <div className="App"></div>;
+  const [list, setList] = useState([]);
+  useEffect(() => {
+    fetch(
+      "https://func-co-terminalservice-dev.azurewebsites.net/api/terminals?code=OTvy9V8paWOTtbs8dEKgSjFaPdnOya74lIHIEwBRU2ao0RHPmkVTKQ==",
+    )
+      .then((response) => response.json())
+      .then((data) => setList(data));
+  }, []);
+  // const {
+  //   register,
+  //   handleSubmit,
+  //   watch,
+  //   formState: { errors },
+  // } = useForm();
+  // const onSubmit = (data: any) => console.log(data);
+  return (
+    <div className={styles.basic}>
+      {/* <form onSubmit={handleSubmit(onSubmit)}> */}
+      {list.map((data, i) => (
+        <>
+          <Input label={data} key={i} />
+        </>
+      ))}
+
+      {/* <input type="submit" /> */}
+      {/* </form> */}
+    </div>
+  );
 }
